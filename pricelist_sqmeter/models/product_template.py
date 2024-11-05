c042eacabe206140fbf1c7717beba0d8187f1a80
Alexander Klockhaus <a.klockhaus@gmail.com>
2024-11-05 13:22:26 +0100

    Update product_template.py

--- pricelist_sqmeter/models/product_template.py
+++ pricelist_sqmeter/models/product_template.py
@@ -21,38 +21,56 @@ class ProductTemplate(models.Model):
         
         return combination_info
 
-
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     price_per_sqm = fields.Float(string='Price per Squaremeter', compute='_compute_price_per_sqm')
     display_sqm = fields.Boolean(related='product_tmpl_id.display_sqm')
 
+    @api.depends('list_price', 'product_template_attribute_value_ids')
     def _compute_price_per_sqm(self):
-        """Compute the price per square meter including proper tax calculation"""
         for item in self:
             sqm = 0
+            # Debug log
+            _logger.info(f'Computing price_per_sqm for product {item.name}')
+            
             for record in item.product_template_attribute_value_ids:
+                _logger.info(f'Checking attribute: {record.attribute_id.name}, is_sqm: {record.attribute_id.is_sqm}')
                 if record.product_attribute_value_id.attribute_id.is_sqm:
                     sqm = record.product_attribute_value_id.sqm
+                    _logger.info(f'Found sqm value: {sqm}')
                     break
             
             if sqm:
-                # Get combination info which includes proper tax calculation
-                combination_info = item.product_tmpl_id._get_combination_info(
-                    product_id=item.id,
-                    add_qty=1,
-                    only_template=False,
-                )
+                # Get the price directly from the product first
+                price = item.list_price
+                _logger.info(f'Initial price: {price}')
                 
-                # Use the properly calculated price from combination_info
-                # This price already includes taxes based on Odoo's configuration
-                price = combination_info['price']
+                # Get website
+                website = self.env['website'].get_current_website()
+                
+                # Get the pricelist
+                pricelist = website.pricelist_id
+                if pricelist:
+                    price = pricelist._get_product_price(item, 1.0, currency=website.currency_id)
+                    _logger.info(f'Price after pricelist: {price}')
+                
+                # Get taxes
+                taxes = item.taxes_id.filtered(lambda t: t.company_id == self.env.company)
+                if taxes:
+                    fpos = website.fiscal_position_id
+                    if fpos:
+                        taxes = fpos.map_tax(taxes)
+                    price_taxed = taxes.compute_all(price, website.currency_id, 1.0, item)['total_included']
+                    _logger.info(f'Price after taxes: {price_taxed}')
+                    price = price_taxed
                 
                 # Calculate price per sqm
                 item.price_per_sqm = price / sqm if sqm else 0
+                _logger.info(f'Final price_per_sqm: {item.price_per_sqm}')
             else:
                 item.price_per_sqm = 0
+                _logger.info('No sqm value found, setting price_per_sqm to 0')
 
 
 class ProductAttribute(models.Model):
